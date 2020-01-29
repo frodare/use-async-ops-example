f9e798c12e22c409342fbca67532f60f332ae2be
Charles Howard <frodare@gmail.com>
2020-01-28 21:26:11 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { register, useAsyncOp, registerPlugin } from 'use-async-ops'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import RunningOpsProvider from 'use-async-ops/dist/plugins/runningOps/RunningOpsProvider'
+import useRunningOps from 'use-async-ops/dist/plugins/runningOps/useRunningOps'
+
+// import { reduxPlugin } from 'use-async-ops-redux'
+// import useAsyncRunning from 'use-async-ops/dist/useAsyncRunning'
+
+const delay = time => new Promise(resolve => window.setTimeout(resolve, time))
+
+register('op1', async id => {
+  await delay(3000)
+  return 'response for ' + id
+})
+
+const reducer = (state = {}, action) => state
+
+const store = createStore(reducer)
+
+// registerPlugin(reduxPlugin(store.dispatch))
+
+const ShowRunning = () => {
+  const running = useRunningOps()
+  return <pre>{JSON.stringify(running, null, 2)}</pre>
 }
 
-export default App;
+export default () => {
+  const { call, result, loading } = useAsyncOp('op1')
+
+  return (
+    <RunningOpsProvider registerPlugin={registerPlugin}>
+      <Provider store={store}>
+        <div>
+          <button onClick={() => call(1)}>run</button>
+          <pre>{JSON.stringify({ result, loading }, null, 2)}</pre>
+          <ShowRunning />
+        </div>
+      </Provider>
+    </RunningOpsProvider>
+  )
+}
