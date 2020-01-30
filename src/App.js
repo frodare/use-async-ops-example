b284db3273ea34bb760898695b4a66633195b862
Charles Howard <frodare@gmail.com>
2020-01-29 21:53:54 -0500

    switching to middlewares

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { register, useAsyncOp, registerPlugin } from 'use-async-ops'
+import { register, useAsyncOp, prependMiddleware } from 'use-async-ops'
 
-import RunningOpsProvider from 'use-async-ops/dist/plugins/runningOps/RunningOpsProvider'
-import useRunningOps from 'use-async-ops/dist/plugins/runningOps/useRunningOps'
-import useAsyncLoading from 'use-async-ops/dist/plugins/runningOps/useAsyncLoading'
+import RunningOpsProvider from 'use-async-ops/dist/middleware/runningOps/RunningOpsProvider'
+import useRunningOps from 'use-async-ops/dist/middleware/runningOps/useRunningOps'
+import useAsyncLoading from 'use-async-ops/dist/middleware/runningOps/useAsyncLoading'
 
 // import { reduxPlugin } from 'use-async-ops-redux'
 // import useAsyncRunning from 'use-async-ops/dist/useAsyncRunning'
@@ -13,8 +13,10 @@ import useAsyncLoading from 'use-async-ops/dist/plugins/runningOps/useAsyncLoadi
 const delay = time => new Promise(resolve => window.setTimeout(resolve, time))
 
 register('op1', async id => {
-  await delay(3000)
+  await delay(1000)
   return 'response for ' + id
+}, {
+  mock: async id => 'MOCK response for ' + id
 })
 
 const reducer = (state = {}, action) => state
@@ -26,20 +28,19 @@ const store = createStore(reducer)
 const ShowRunning = () => {
   const running = useRunningOps()
   const loading = useAsyncLoading()
-  return <pre>{JSON.stringify(running, null, 2)}</pre>
+  return <pre>{JSON.stringify(running, null, 2)} {loading ? <h1>LOADING</h1> : null}</pre>
 }
 
 export default () => {
   const { call, result, loading } = useAsyncOp('op1')
 
   return (
-    <RunningOpsProvider registerPlugin={registerPlugin}>
+    <RunningOpsProvider prependMiddleware={prependMiddleware}>
       <Provider store={store}>
         <div>
           <button onClick={() => call(1)}>run</button>
           <pre>{JSON.stringify({ result, loading }, null, 2)}</pre>
           <ShowRunning />
-          {loading ? <h1>LOADING</h1> : null}
         </div>
       </Provider>
     </RunningOpsProvider>
