db7302f5c9568fe0dbd5a7dcd47436bc3a01c28c
Charles Howard <frodare@gmail.com>
2020-03-23 16:29:28 -0400

    useAsyncEffect update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { register, useAsyncOp, prependMiddleware } from 'use-async-ops'
-
-import RunningOpsProvider from 'use-async-ops/dist/middleware/runningOps/RunningOpsProvider'
-import useRunningOps from 'use-async-ops/dist/middleware/runningOps/useRunningOps'
-import useAsyncLoading from 'use-async-ops/dist/middleware/runningOps/useAsyncLoading'
-
-// import { reduxPlugin } from 'use-async-ops-redux'
-// import useAsyncRunning from 'use-async-ops/dist/useAsyncRunning'
+import { register, useAsyncOp, useAsyncEffect, prependMiddleware, middleware } from 'use-async-ops'
+import { reduxMiddleware, reducer } from 'use-async-ops-redux'
+import logger from 'redux-logger'
 
 const delay = time => new Promise(resolve => window.setTimeout(resolve, time))
 
@@ -19,30 +14,38 @@ register('op1', async id => {
   mock: async id => 'MOCK response for ' + id
 })
 
-const reducer = (state = {}, action) => state
+// const reducer = (state = {}, action) => reducer
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 
-// registerPlugin(reduxPlugin(store.dispatch))
+// prependMiddleware(middleware.logging)
+prependMiddleware(reduxMiddleware(store.dispatch))
 
 const ShowRunning = () => {
-  const running = useRunningOps()
-  const loading = useAsyncLoading()
+  const running = middleware.runningOps.useRunningOps()
+  const loading = middleware.runningOps.useAsyncLoading()
   return <pre>{JSON.stringify(running, null, 2)} {loading ? <h1>LOADING</h1> : null}</pre>
 }
 
 export default () => {
-  const { call, result, loading } = useAsyncOp('op1')
+  const { call, result, loading } = useAsyncOp({ name: 'op1', options: { channel: '1' } })
+  const { call: call2, result: result2, loading: loading2 } = useAsyncOp({ name: 'op1', options: { channel: '2' } })
+  useAsyncEffect({ name: 'op1', options: { channel: 'autoStart' } }, 1, 2, 3)
 
   return (
-    <RunningOpsProvider prependMiddleware={prependMiddleware}>
+    <middleware.runningOps.Provider prependMiddleware={prependMiddleware}>
       <Provider store={store}>
         <div>
           <button onClick={() => call(1)}>run</button>
           <pre>{JSON.stringify({ result, loading }, null, 2)}</pre>
-          <ShowRunning />
         </div>
+
+        <div>
+          <button onClick={() => call2(1)}>run</button>
+          <pre>{JSON.stringify({ result2, loading2 }, null, 2)}</pre>
+        </div>
+        <ShowRunning />
       </Provider>
-    </RunningOpsProvider>
+    </middleware.runningOps.Provider>
   )
 }
